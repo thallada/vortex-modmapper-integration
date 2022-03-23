d67e75af4af940542b7a1d1ff9c070b47703789d
Tyler Hallada <tyler@hallada.net>
2022-03-23 18:56:49 -0400

    Rename project and fix sourcemap collision

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,18 @@
 let webpack = require('vortex-api/bin/webpack').default;
 const WebpackDynamicPublicPathPlugin = require("webpack-dynamic-public-path");
 
-const config = webpack('modmapper-integration', __dirname, 4);
+const config = webpack('vortex-modmapper-integration', __dirname, 4);
 
 config.resolve.extensions.push(".wasm");
-config.entry = { app: "./src/index.ts" };
+config.entry = { "vortex-modmapper-integration": "./src/index.ts" };
 
 config.output.publicPath = "publicPathPlaceholder";
 // hack to get webpack to load chunks and wasm from the vortex extension directory
 config.plugins.push(new WebpackDynamicPublicPathPlugin({
   externalPublicPath: `"file:///" + __dirname + "/"`,
-  chunkNames: ["app"],
+  chunkNames: ["vortex-modmapper-integration"],
 }));
+
+config.output.sourceMapFilename = `[name].js.map`
 
 module.exports = config;
